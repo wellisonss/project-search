eec3e3143f9f3bca8e49ca21b623e2ed0bc93fbe
Wellison Santos <silva.s.wellison@gmail.com>
2026-04-13 09:42:16 -0300

    adiciona ConfiguracaoBusca ao módulo de busca: atualiza importações para incluir a entidade ConfiguracaoBusca

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { HistoricoBusca } from './historico.entity';
+import { ConfiguracaoBusca } from './configuracao.entity'; 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HistoricoBusca])],
+  imports: [
+    TypeOrmModule.forFeature([HistoricoBusca, ConfiguracaoBusca]) 
+  ],
   providers: [SearchService],
   controllers: [SearchController]
 })
